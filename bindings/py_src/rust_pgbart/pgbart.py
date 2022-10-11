5f771438553b37b94ce82236e2706e0749f0c9e3
Marcin Elantkowski <marcin.elantkowski@gmail.com>
2022-10-11 19:46:30 +0200

    Re-write pgbart.py to use the new wrapper

--- bindings/py_src/rust_pgbart/pgbart.py
+++ bindings/py_src/rust_pgbart/pgbart.py
@@ -1,47 +1,18 @@
-#   Copyright 2022 The PyMC Developers
-#
-#   Licensed under the Apache License, Version 2.0 (the "License");
-#   you may not use this file except in compliance with the License.
-#   You may obtain a copy of the License at
-#
-#       http://www.apache.org/licenses/LICENSE-2.0
-#
-#   Unless required by applicable law or agreed to in writing, software
-#   distributed under the License is distributed on an "AS IS" BASIS,
-#   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-#   See the License for the specific language governing permissions and
-#   limitations under the License.
+# The code in this file is based on 
+# https://github.com/pymc-devs/pymc-bart/blob/0f0e3617ac03877448f5eded315e8cb810d1d0cb/pymc_bart/pgbart.py
 
-from time import perf_counter, time
+from time import perf_counter
 
-import aesara
-import aesara.link.numba.dispatch
 import numpy as np
-from aeppl.logprob import CheckParameterValue
-from aesara import function as aesara_function  # type: ignore
-from numba import carray, cfunc, literal_unroll, njit, types
-from pymc.aesaraf import inputvars, join_nonshared_inputs, make_shared_replacements
+from pymc.aesaraf import inputvars
 from pymc.model import modelcontext
 from pymc.step_methods.arraystep import ArrayStepShared, Competence
 
 from rust_pgbart.bart import BARTRV
+from rust_pgbart.logp import ModelLogPWrapper
+
+# Defined in Rust
 from rust_pgbart.rust_pgbart import initialize, step
-
-
-# Provide a numba implementation for CheckParameterValue,
-# which doesn't exist in aesara
-@aesara.link.numba.dispatch.basic.numba_funcify.register(CheckParameterValue)
-def numba_functify_CheckParameterValue(op, **kwargs):
-    msg = f"Invalid parameter value {str(op)}"
-
-    @aesara.link.numba.dispatch.basic.numba_njit
-    def check(value, *conditions):
-        for cond in literal_unroll(conditions):
-            if not cond:
-                raise ValueError(msg)
-        return value
-
-    return check
 
 
 class PGBART(ArrayStepShared):
@@ -65,7 +36,7 @@ class PGBART(ArrayStepShared):
     name = "pgbart"
     default_blocked = False
     generates_stats = True
-    stats_dtypes = [{"Time": object, "N calls": object}]
+    stats_dtypes = [{"time_py": float, "time_rs": float}]
 
     def __init__(
         self,
@@ -74,16 +45,11 @@ class PGBART(ArrayStepShared):
         batch=(0.1, 0.1),
         model=None,
     ):
+        # PyMC setup
         model = modelcontext(model)
-        initial_values = model.initial_point()
-        if vars is None:
-            vars = model.value_vars
-        else:
-            vars = [model.rvs_to_values.get(var, var) for var in vars]
-            vars = inputvars(vars)
-        value_bart = vars[0]
 
-        self.bart = model.values_to_rvs[value_bart].owner.op
+        # Get the data and params from the BART instance
+        self.bart = self.get_bart_rv(model, vars)
         X = self.bart.X
         y = self.bart.Y
         m = self.bart.m
@@ -94,19 +60,14 @@ class PGBART(ArrayStepShared):
         if np.any(np.isnan(X)):
             raise NotImplementedError()
 
-        # TODO(elantkom): generalize the code to mutlivariate inputs?
-        shape = initial_values[value_bart.name].shape  # type: ignore
-        assert len(shape) == 1
-        shape = 1
+        # Generate the log-p C function that can be passed to Rust
+        self.logp_wrapper = ModelLogPWrapper(model, vars)
 
-        shared = make_shared_replacements(initial_values, vars, model)
-        super().__init__(vars, shared)
-
-        numba_fn = make_numba_fn(initial_values, [model.datalogp], vars, shared)
+        # Intialize the Rust sampler
         self.state = initialize(
             X=X,
             y=y,
-            logp=numba_fn.address,
+            logp=self.logp_wrapper.logp_function_pointer(),
             alpha=alpha,
             n_trees=m,
             n_particles=num_particles,
@@ -116,16 +77,36 @@ class PGBART(ArrayStepShared):
         )
 
         self.tune = True
+        super().__init__(vars, self.logp_wrapper.shared)
+
+    def get_bart_rv(self, model, vars):
+        """ Get the instance of BART RV from the PyMC model
+        """
+
+        if vars is None:
+            vars = model.value_vars
+        else:
+            vars = [model.rvs_to_values.get(var, var) for var in vars]
+            vars = inputvars(vars)
+        value_bart = vars[0]
+
+        bart = model.values_to_rvs[value_bart].owner.op
+        return bart
+
 
     def astep(self, _):
-
-        self.logp_call_counter = 0
+        
         t0 = perf_counter()
-
+        
+        self.logp_wrapper.update_persistent_arrays()
+        
+        t1 = perf_counter()
+        
         sum_trees = step(self.state, self.tune)
 
-        stats = {"Time": perf_counter() - t0, "N calls": 0}
-        return sum_trees, [stats]
+        t2 = perf_counter()
+
+        return sum_trees, [{"time_py": t1 - t0, "time_rs": t2 - t1}]
 
     @staticmethod
     def competence(var, has_grad):
@@ -134,31 +115,3 @@ class PGBART(ArrayStepShared):
         if isinstance(dist, BARTRV):
             return Competence.IDEAL
         return Competence.INCOMPATIBLE
-
-
-def make_numba_fn(point, out_vars, vars, shared):  # pylint: disable=redefined-builtin
-    """Compile Aesara function of the model and the input and output variables.
-
-    Parameters
-    ----------
-    out_vars: List
-        containing :class:`pymc.Distribution` for the output variables
-    vars: List
-        containing :class:`pymc.Distribution` for the input variables
-    shared: List
-        containing :class:`aesara.tensor.Tensor` for depended shared data
-    """
-
-    out_list, inarray0 = join_nonshared_inputs(point, out_vars, vars, shared)
-    function = aesara_function([inarray0], out_list[0], mode=aesara.compile.NUMBA)
-    function.trust_input = True
-    full_logp = function.vm.jit_fn
-
-    @cfunc(types.float32(types.CPointer(types.float32), types.intc))
-    @njit
-    def logp(predictions_ptr, predictions_size):
-        arr = carray(predictions_ptr, (predictions_size,), np.float32)
-        ret = full_logp(arr)[0]
-        return ret
-
-    return logp
