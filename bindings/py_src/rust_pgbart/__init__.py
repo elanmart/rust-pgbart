15bf9bdc3aeb16a764ae0ac219c8942f07cbcc11
Marcin Elantkowski <marcin.elantkowski@gmail.com>
2022-10-11 19:46:19 +0200

    Expose logp wrapper

--- bindings/py_src/rust_pgbart/__init__.py
+++ bindings/py_src/rust_pgbart/__init__.py
@@ -1,6 +1,7 @@
 import pymc as pm
 
 from rust_pgbart.bart import BART
+from rust_pgbart.logp import ModelLogPWrapper
 from rust_pgbart.pgbart import PGBART
 
 pm.STEP_METHODS = list(pm.STEP_METHODS) + [PGBART]
@@ -8,4 +9,5 @@ pm.STEP_METHODS = list(pm.STEP_METHODS) + [PGBART]
 __all__ = [
     "BART",
     "PGBART",
+    "ModelLogPWrapper",
 ]
